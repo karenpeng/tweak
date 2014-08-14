b01b46343cc0d5b4f58c72600963d15011c8273e
Karen <karenpenglabs@gmail.com>
2014-08-13 22:25:57 -0400

    new beginning

--- sketch.js
+++ sketch.js
@@ -1,30 +1,118 @@
-	window.onload = function () {
-	  // Get a reference to the canvas object
-	  var canvas = document.getElementById('myCanvas');
-	  // Create an empty project and a view for the canvas:
-	  paper.setup(canvas);
-	  // Create a Paper.js Path to draw a line into it:
-	  var path = new paper.Path();
-	  // Give the stroke a color
-	  path.strokeColor = 'black';
-	  var start = new paper.Point(100, 100);
-	  // Move to start and draw a line from there
-	  path.moveTo(start);
-	  // Note that the plus operator on Point objects does not work
-	  // in JavaScript. Instead, we need to call the add() function:
-	  path.lineTo(start.add([200, -50]));
-	  // Draw the view now:
-	  paper.view.draw();
-	}
+paper.install(window);
 
-	function hell() {
-	  //this is about making a path
-	}
+//making a path
+window.onload = function () {
 
-	function whatever() {
-	  //this is about getting value from the path
-	}
+  var path;
+  var paths = [];
 
-	function hookup() {
-	  //this is about hookup to control animations
-	}
+  paper.setup('myCanvas');
+  var width = document.getElementById('myCanvas').width;
+
+  var tool = new Tool();
+
+  var segment;
+  var hitOptions = {
+    segments: true,
+    stroke: true,
+    fill: false,
+    tolerance: 5
+  };
+
+  tool.onMouseDown = function (e) {
+    if (isDrawingMode) {
+      path = new Path();
+      path.strokeColor = {
+        gradient: {
+          stops: ['blue', 'red', 'yellow']
+        },
+        origin: [0, 0],
+        destination: [width, 0]
+      };
+      path.strokeWidth = 15;
+    } else {
+      segment = null;
+      path = null;
+      var hitResult = project.hitTest(e.point, hitOptions);
+      if (!hitResult) {
+        return;
+      }
+      if (e.modifiers.shift) {
+        if (hitResult.type === 'segment') {
+          hitResult.segment.remove();
+        };
+        return;
+      }
+      if (hitResult) {
+        path = hitResult.item;
+        if (hitResult.type === 'segment') {
+          segment = hitResult.segment;
+        } else if (hitResult.type === 'stroke') {
+          var location = hitResult.location;
+          segment = path.insert(location.index + 1, e.point);
+          path.smooth();
+        }
+      }
+    }
+  };
+
+  tool.onMouseDrag = function (e) {
+    if (isDrawingMode) {
+      path.add(e.point);
+    } else if (segment) {
+      //console.log(segment.point.x+ " with" + e.delta.x)
+      segment.point.x += e.delta.x;
+      segment.point.y += e.delta.y;
+      path.smooth();
+    } else if (path) {
+      console.log(path)
+      path.position += e.delta;
+    }
+  };
+
+  tool.onMouseUp = function (e) {
+    if (isDrawingMode) {
+      path.smooth();
+      path.simplify();
+      paths.push(path);
+    }
+  };
+
+  tool.onMouseMove = function (e) {
+    if (isDrawingMode) {
+
+    } else {
+      project.activeLayer.selected = false;
+      if (e.item) {
+        e.item.selected = true;
+      }
+    }
+  };
+
+  //gatting value
+  function getValue(pathP) {
+    for (var i = 0; i < pathP.curves.length; i++) {
+      var curve = pathP.curves[i];
+      var interval = 5;
+      //console.log(curve.length)
+      for (var j = 1; j <= curve.length; j += interval) {
+        var curvePosition = curve.getLocationAt(j / curve.length);
+        //console.log(curvePosition.point.x, curvePosition.point.y)
+      }
+    }
+  }
+  //TODO:
+  //0.add begin and end point
+
+  //1.hooking up for use, css3!!!!
+
+  //2.browserify! T T  T T
+
+  //3.i think it should involves live coding, the code control all the canvas
+
+  //4.generative design? Or just a ball?
+
+  tool.onKeyDown = function (e) {
+    getValue(path)
+  }
+};
