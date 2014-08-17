22e4bf6dcd626f6679d903a938f61a3ff813c8a4
Karen <karenpenglabs@gmail.com>
2014-08-17 16:30:53 -0400

    question:which epoint and stuff

---
+++ test.js
@@ -0,0 +1,15 @@
+var uniq = require('uniq');
+var can = require("./can");
+//paper.install(window);
+//var i = 0;
+window.onload = function () {
+  var canvasGroup = document.getElementById('canvasGroup');
+  for (var itr = 0; itr < 4; itr++) {
+    can(canvasGroup, itr * 320);
+  }
+}
+
+// var canvasGroup = document.getElementById('canvasGroup');
+// var drawCanvas = document.createElement('canvas');
+// drawCanvas.setAttribute('id', 'drawCanvas' + 0);
+// canvasGroup.appendChild(drawCanvas);
