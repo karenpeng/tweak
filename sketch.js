581a0c979b8149a85afe46b42556521452b2b8a8
Karen <karenpenglabs@gmail.com>
2014-08-16 20:01:51 -0400

    try use intersection to get y value every x interval

--- sketch.js
+++ sketch.js
@@ -4,17 +4,19 @@ var beginX = 20;
 var beginY = 40;
 var endX = 640 - 20;
 var endY = 480 - 40;
+var interval = 20;
+var radius = 20;
 
 //making a path
 //window.onload = function () {
 function whatever() {
   var path;
-  var paths = [];
+  //var paths = [];
   //TODO: paper is weird, it seems have done something on the canvas
   paper.setup('drawCanvas');
-  var width = drawCanvas.width;
-  var height = drawCanvas.height;
-  console.log(width, height)
+  //var width = drawCanvas.width;
+  //var height = drawCanvas.height;
+  //console.log(width, height)
   var tool = new Tool();
 
   var segment;
@@ -25,14 +27,9 @@ function whatever() {
     tolerance: 5
   };
 
-  //var drawArea = new Path.Rectangle(10, 10, width / 2, width / 2);
-  // var animiArea = new Path.Rectangle(width / 4 + 10, 10, width / 4, width / 4);
-  //drawArea.strokeColor = 'black';
-  // animiArea.strokeColor = 'black';
-
-  var beginP = new Path.Circle(new Point(20, 40), 20);
+  var beginP = new Path.Circle(new Point(20, 40), radius);
   var endP = new Path.Circle(new Point(640 - 20, 480 - 40),
-    20);
+    radius);
   beginP.strokeColor = 'black';
   endP.strokeColor = 'black';
 
@@ -79,6 +76,12 @@ function whatever() {
   tool.onMouseDrag = function (e) {
     if (isDrawingMode) {
       //if (drawArea.bounds.contains(e.point)) {
+      path.segments.forEach(function (s, index) {
+        if (s.point.x >= e.point.x) {
+          path.removeSegment(index);
+        }
+
+      })
       path.add(e.point);
       if (endP.bounds.contains(e.point)) {
         endP.fillColor = "pink";
@@ -96,12 +99,15 @@ function whatever() {
   };
 
   tool.onMouseUp = function (e) {
-    if (isDrawingMode && endP.bounds.contains(e.point)) {
+    if (isDrawingMode) {
+      if (!endP.bounds.contains(e.point)) {
+        path.add(new Point(endX, endY));
+      }
       endP.fillColor = 'pink';
       path.smooth();
       path.simplify();
       // paths.push(path);
-      var value = getValue(path)
+      var value = getValue()
       console.log(value)
       // createAnime(function () {
       //   moveBall(value);
@@ -120,20 +126,38 @@ function whatever() {
     }
   };
 
+  //making vertival x path
+  var verticalPaths = [];
+  for (var i = beginX; i <= endX; i += interval) {
+    var verticalPath = new Path.Line(new Point(i, beginY), new Point(i, endY));
+    verticalPath.strokeColor = 'black';
+    verticalPaths.push(verticalPath);
+  }
   //gatting value
-  function getValue(pathP) {
-    var curvePoints = [];
-    for (var i = 0; i < pathP.curves.length; i++) {
-      var curve = pathP.curves[i];
-      var interval = 10;
-      //console.log(curve.length)
-      for (var j = 1; j <= curve.length; j += interval) {
-        var curvePosition = curve.getLocationAt(j / curve.length);
-        //console.log(curvePosition.point.x, curvePosition.point.y)
-        curvePoints.push([curvePosition.point.x, curvePosition.point.y]);
-      }
-    }
-    return curvePoints;
+  function getValue() {
+
+    // var curvePoints = [];
+    // for (var i = 0; i < pathP.curves.length; i++) {
+    //   var curve = pathP.curves[i];
+    //   var interval = 10;
+    //   //console.log(curve.length)
+    //   for (var j = 1; j <= curve.length; j += interval) {
+    //     var curvePosition = curve.getLocationAt(j / curve.length);
+    //     //console.log(curvePosition.point.x, curvePosition.point.y)
+    //     curvePoints.push([curvePosition.point.x, curvePosition.point.y]);
+    //   }
+    // }
+    var j = 0;
+    var value = [];
+    verticalPaths.forEach(function (pa) {
+      var intersections = path.getIntersections(pa);
+      intersections.forEach(function (intersection) {
+        console.log(j, intersection.point)
+        j++
+        value.push([intersection.point.x, intersection.point.y])
+      })
+    })
+    return value;
   }
   //TODO:
   //0.add begin and end point
@@ -175,7 +199,7 @@ function Ball(beginX, beginY, endX, endY) {
   this.endX = endX;
   this.endY = endY;
 }
-ball.prototype.draw = function () {
+Ball.prototype.draw = function () {
   animiCtx.beginPath();
   animiCtx.arc(this.x, this.y, 40, 0, Math.PI * 2);
   animiCtx.fillStyle = "#ddbb00";
