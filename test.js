e629a09b675b2c9e3c08dbe4df3b5d3dce16cc98
Karen <karenpenglabs@gmail.com>
2014-08-19 09:05:24 -0400

    fail to use watchify

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-var uniq = require('uniq');
+var watchify = require('watchify');
 var can = require("./can");
+
 //paper.install(window);
-//var i = 0;
-window.onload = function () {
-  var canvasGroup = document.getElementById('canvasGroup');
-  for (var itr = 0; itr < 4; itr++) {
-    can(canvasGroup, itr * 320);
-  }
+//var i = 0; window.onload = function () {
+var canvasGroup = document.getElementById('canvasGroup');
+for (var itr = 0; itr < 4; itr++) {
+  can(canvasGroup, itr * 320);
 }
 
 // var canvasGroup = document.getElementById('canvasGroup');
