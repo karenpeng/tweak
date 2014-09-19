12fc82bed2d8c7c9e272ada70bd6f7f95e6acbd3
Karen <karenpenglabs@gmail.com>
2014-09-18 23:29:28 -0400

    i can't give up

--- parser.js
+++ parser.js
@@ -5,12 +5,30 @@ function Parser() {
 
 }
 
-Parser.prototype.dictionary = function () {
-  'y': function () {}
-  'x': function () {}
-  'size': function () {}
+Parser.prototype.dictionary = function (word) {
+  switch (word) {
+  case 'y':
+    function () {}
+    break;
+  case 'x':
+    function () {}
+    break;
+  case 'size':
+    function () {}
+    break;
+
+  }
 };
 
-Parser.prototype.eval = function () {
+Parser.prototype.eval = function (source) {
+  if () {
+    //stack?
+    //what?
+  }
 
 };
+
+circle
+y
+from 20 to 50 in 2 seconds
+with each path in paths
